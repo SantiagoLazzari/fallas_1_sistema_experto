ecbbcaa38dc81a2e603d7ef7bf59ece7d49ac215
SantiagoLazzari <slazzari@fi.uba.ar>
2020-07-25 17:42:33 -0300

    Added bariloche and mardel

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ from random import choice
 from experta import *
 
 
-class Light(Fact):
-    """Info about the traffic light."""
+class PreferedLandscape(Fact):
+    """
+    Info about the prefered landscape.
+    possible options:
+    - mountain
+    - beach
+    - city
+    """
     pass
 
 
-class RobotCrossStreet(KnowledgeEngine):
-    @Rule(Light(color='green'))
-    def green_light(self):
-        print("Walk")
+class TuristAgent(KnowledgeEngine):
+    @Rule(PreferedLandscape(landscape='mountain'))
+    def mountain(self):
+        print("Bariloche")
 
-    @Rule(Light(color='red'))
+    @Rule(PreferedLandscape(landscape='beach'))
     def red_light(self):
-        print("Don't walk")
+        print("Mar del plata")
 
-    @Rule(AS.light << Light(color=L('yellow') | L('blinking-yellow')))
-    def cautious(self, light):
-        print("Be cautious because light is", light["color"])
-
-engine = RobotCrossStreet()
+engine = TuristAgent()
 engine.reset()
-engine.declare(Light(color=choice(['green', 'yellow', 'blinking-yellow', 'red'])))
+prefered_landscape = PreferedLandscape(landscape = 'beach')
+engine.declare(prefered_landscape)
 engine.run()
